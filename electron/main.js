bc2fb3d46c6dba251d64082638f7dc4664d1e010
xioxin <i@xioxin.com>
2026-03-12 23:57:01 +0800

    启用节点集成以支持渲染进程中的模块访问

--- electron/main.js
+++ electron/main.js
@@ -18,7 +18,7 @@ function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
-      nodeIntegration: false,
+      nodeIntegration: true,
     },
   })
 
